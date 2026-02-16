812f362f65ace7981ec1e1c5fad067bdfbbeb346
n-szilard <nagyapatiszilard@turr.hu>
2026-02-16 10:33:34 +0100

    Új doboz form

--- src/app/components/new-box/new-box.component.ts
+++ src/app/components/new-box/new-box.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { Box } from '../../interfaces/box';
+import { SelectButton } from 'primeng/selectbutton';
+import { ButtonModule } from 'primeng/button';
+import { TextareaModule } from 'primeng/textarea';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-new-box',
   standalone: true,
-  imports: [],
+  imports: [DropdownModule, InputTextModule, InputNumberModule, FormsModule, CommonModule, SelectButton, ButtonModule, TextareaModule, FloatLabelModule],
   templateUrl: './new-box.component.html',
   styleUrl: './new-box.component.scss'
 })
 export class NewBoxComponent {
+  model: Box = {
+    userId: '',
+    code: '',
+    labelType: 'QR',
+    lengthCm: 0,
+    widthCm: 0,
+    heightCm: 0,
+    maxWeightKg: 0,
+    location: '',
+    note: '',
+    status: 'ACTIVE',
+  }
 
+  onCancel() {
+
+  }
+
+  onSubmit() {
+
+  }
 }
---
+++ src/app/interfaces/box.ts
@@ -0,0 +1,15 @@
+export interface Box {
+    id?: string,
+    userId?: string,
+    code: string,
+    labelType: 'QR' | 'BARCODE',
+    lengthCm: number,
+    widthCm: number,
+    heightCm: number,
+    maxWeightKg: number,
+    location: string,
+    note: string,
+    status: 'ACTIVE' | 'ARCHIVED' | 'DAMAGED',
+    createdAt?: Date,
+    updatedAt?: Date
+}
