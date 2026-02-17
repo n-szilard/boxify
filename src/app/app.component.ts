23d8bd3446bb2013699d33e80f434da978dd741a
n-szilard <nagyapatiszilard@turr.hu>
2026-02-17 12:01:17 +0100

    Doboz törlés

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { Toast } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent, Toast],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent, Toast, ConfirmDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
